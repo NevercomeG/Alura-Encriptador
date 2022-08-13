2aa11417a542801782fe5da78a6ee7aa07fb579c
NevercomeG <luisjavier2012@gmail.com>
2022-08-12 22:34:41 -0400

    1.0

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -1,23 +1,47 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-import Inputgroup from "react-bootstrap/InputGroup";
+import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
 
-export default function TodoForm(){
+export default function TodoForm({ todos, setTodos }) {
+    const [name, setName] = useState("");
 
-        const [name, setName] = useState("");
+    const handleChange = e => {
+        setName(e.target.value);
+    }
 
-        const handleChange = e => {
-            setName(e.target.value);
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!name) {
+            alert("Please provide a valid value for todo");
+            return;
         }
 
-    return <Form>
-        <Inputgroup clasName =" mb-4">
-            <FormControl placeholder="New Todo" onchange={handleChange} value={name}/>
+        axios.post("/api/todos/", {
+            name: name
+        }).then((res) => {
+            setName("");
+            const { data } = res;
+            setTodos([
+                ...todos,
+                data
+            ]).catch(() => {
+                alert("Something went wrong");
+            })
+        })
+    }
+
+    return <Form onSubmit={handleSubmit}>
+        <InputGroup className="mb-4">
+            <FormControl placeholder="New Todo"
+                onChange={handleChange}
+                value={name}
+            />
             <Button type="submit">
                 Add
             </Button>
-        </Inputgroup>
+        </InputGroup>
     </Form>
 }
