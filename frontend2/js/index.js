7369dc67eff111993c42bc79abaaaebc963b8c27
NevercomeG <luisjavier2012@gmail.com>
2022-08-14 23:47:49 -0400

    1.45a

--- frontend2/js/index.js
+++ frontend2/js/index.js
@@ -2,10 +2,6 @@ import "./style.css"
 
 
 
-function Encryptor() {
-
-    
-
 function Encriptar(){
 
     var texto = document.querySelector("#input-texto").value;
@@ -23,32 +19,3 @@ function desencriptar (){
         document.querySelector("#input-texto").value;
         var boton2 = document.querySelector("#btn-desencriptar"); boton2.onclick = desencriptar;
 }
-
-    return (
-        <section id ="main">
-            <div className="Encryptor">
-
-                <form action="">
-                <p><label for="w3review">Encriptar</label></p>
-                <textarea id="w3review" name="w3review" rows="20" cols="70">Encriptar</textarea>
-                <input type="submit" value="Submit"/>
-                </form>
-                </div>
-            <div className="Encryptor2">
-
-                
-            <form action="">
-                <p><label for="w3review">Desencriptar</label></p>
-                <textarea id="w3review" name="w3review" rows="20" cols="70">Desencriptar</textarea>
-                <input type="submit" value="Submit"/>
-                </form>
-
-            </div>
-
-        </section>
-    )
-}
-
-
-export default Encryptor; 
-
