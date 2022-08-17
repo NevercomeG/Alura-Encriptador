25d7b2c2058da1db12f88e29b07fc7a3a7d6bce5
NevercomeG <luisjavier2012@gmail.com>
2022-08-17 14:25:04 -0400

    Js done

--- frontend2/src/index.js
+++ frontend2/src/index.js
@@ -1,21 +1,19 @@
-import "./style.css"
 
+function encriptar(){
 
-
-function Encriptar(){
-
-    var texto = document.querySelector("#input-texto").value;
+    var texto = document.querySelector("#textodescriptado").value.toLowerCase();
     var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
-    document.querySelector(".text-input-salida").value = textoCifrado;
-    document.querySelector("#input-texto").value;
-    var encriptar
-    var boton1 = document.querySelector("#btn-encriptar"); boton1.onclick = encriptar;
+    document.querySelector("#textoencriptado").value = textoCifrado;
+    document.querySelector("#textodescriptado").value = "";
+
+
+
 }
 
 function desencriptar (){ 
-        var texto = document.querySelector("#input-texto").value; 
+        var texto = document.querySelector("#textoencriptado").value; 
         var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
-        document.querySelector(".text-input-salida").value = textoCifrado; 
-        document.querySelector("#input-texto").value;
-        var boton2 = document.querySelector("#btn-desencriptar"); boton2.onclick = desencriptar;
+        document.querySelector("#textodescriptado").value = textoCifrado; 
+        document.querySelector("#textoencriptado").value = "";
+
 }
